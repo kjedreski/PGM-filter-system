b04d82e7f1ca4e3e7bbaa47a5a0c5ff5b80dd674
Kevin Jedreski <jetski07@gmail.com>
2015-04-14 09:55:09 -0400

    added pgm image

--- Data_Strucs_FInal/InsertionSort.h
+++ Data_Strucs_FInal/InsertionSort.h
@@ -3,13 +3,33 @@
 
 class InsertionSort{
 
+// load text file 
+
+// put text file into 2d array
+
+
+// iterate through 2d array, with a NxN window.
+// For each pixel, sort all pixels in the n by n window
+// Check to see if NxN is even or odd
+// find middle value after NxN is sorted
+// replace the pixel where the window is centered on with the median.
+// Watch out for corners and edges
 
 
 
 
+
+
+
+
+//The processing procedure works as follows. For each pixel of an image, you consider an (n x n) window centered on that pixel, 
+	//compute the median of gray-level values in the window, and the median becomes the new gray-level value of the new image.
+	//The median is defined as the middle value in a sorted sequence. For example, consider the following (3 x 3) window of pixels:
 
 
 
 
 
 };
+
+#endif
---
+++ Data_Strucs_FInal/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "InsertionSort.h"
+
+int main(){
+	// load file into 2d array
+
+
+
+	std::cout << "working fine" << std::endl;
+
+
+
+
+
+
+
+
+
+	return 0;
+}
